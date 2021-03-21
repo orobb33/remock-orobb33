934d3a739b30038de4f46e3b22c15db9061e14e8
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-03-21 17:46:32 -0400

    buttons working

--- server.js
+++ server.js
@@ -14,9 +14,14 @@ app.get("/learnmore", (req, res) => {
     res.sendFile(path.resolve("./public/blog-post-page.html"))
 })
 
+app.get("/continuereading", (req, res) => {
+    res.sendFile(path.resolve("./public/continue-reading.html"))
+})
 
 
-
+app.get("/postonly", (req, res) => {
+  res.sendFile(path.resolve("./public/post-only.html"))
+})
 
 
 app.listen(PORT, () => {
